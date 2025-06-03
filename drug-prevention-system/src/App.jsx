b4379fab824bcc01d3047dccb9c50b9fb011e275
urjhn <uyennhi01022004>
2025-06-03 13:49:05 +0700

    hero of homepage

--- drug-prevention-system/src/App.jsx
+++ drug-prevention-system/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import './App.css'
 import Homepage from './pages/Homepage/Homepage'
-function App() {
 
+function App() {
   return (
-    <>
-    <h1>j</h1>
-      <Homepage/>
-    </>
+    <Homepage/>
   )
 }
 
