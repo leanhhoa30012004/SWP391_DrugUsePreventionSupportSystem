fc39760154377cc93b8a47c6be490b3c98a54f46
leanhhoa30012004 <leanhhoa30012004@gmail.com>
2025-06-12 20:27:25 +0700

    manager manage user and login

--- swp391-server/src/routes/survey.routes.js
+++ swp391-server/src/routes/survey.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const survey = require("../app/controllers/survey.controller");
 
-router.get('/viewSurvey', survey.viewSurvey);
-router.get('/typeSurvey', survey.findSurveyByType);
-router.get('/surveyById', survey.findSurveyBySurveyId);
-router.post('/submitSurvey', survey.submitSurvey);
-router.post('/surveyHistory', survey.getSurveyHistoryByMember);
+router.get("/viewSurvey", survey.viewSurvey);
+router.get("/typeSurvey", survey.findSurveyByType);
+router.get("/surveyById", survey.findSurveyBySurveyId);
+router.post("/submitSurvey", survey.submitSurvey);
+router.post("/surveyHistory", survey.getSurveyHistoryByMember);
 
 module.exports = router;
