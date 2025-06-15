26afd0b41c3da76865e7e785645fcdff5fd60e58
leanhhoa30012004 <leanhhoa30012004@gmail.com>
2025-06-15 14:39:05 +0700

    Hoale delete trash

--- swp391-server/src/app/controllers/manager.controllers.js
+++ swp391-server/src/app/controllers/manager.controllers.js
@@ -18,16 +18,7 @@ exports.createUser = async (req, res) => {
 exports.changeProfile = async (req, res) => {
   const { fullname, email, password } = req.body;
   const userId = req.user.user_id; // Assuming userId is set in the request by authentication middleware
-  console.log(
-    "userId:",
-    userId,
-    "fullname:",
-    fullname,
-    "email:",
-    email,
-    "password:",
-    password
-  );
+
   try {
     const result = await managerModels.changeProfile(userId, {
       fullname,
