e2e910e2bf80443e06f4fcdad7f22f519dc4e144
urjhn <uyennhi01022004@gmail.com>
2025-07-01 08:21:21 +0700

    Done course

--- swp391-server/src/app/controllers/course.controller.js
+++ swp391-server/src/app/controllers/course.controller.js
@@ -25,6 +25,7 @@ exports.getCourseByName = async (req, res) => {
 }
 exports.checkEnrollmentCourse = async (req, res) => {
     const { member_id, course_id, enroll_version } = req.params;
+    
     try {
         const check = await courseModel.checkEnrollmentCourse(member_id, course_id, enroll_version);
         res.json({ isEnrolled: check });
@@ -40,22 +41,38 @@ exports.memberContinuesLearnCourseById = async (req, res) => {
         const learning_progress_mooc = course_progress.learning_process;
         const course_content = course_progress.content;
 
+        // Đếm số MOOC đã hoàn thành (score >= 8 và không null/undefined)
+        const moocLength = Array.isArray(learning_progress_mooc) 
+            ? learning_progress_mooc.filter(mooc => mooc && mooc.totalScore >= 8).length 
+            : 0;
 
-        const moocLength = Array.isArray(learning_progress_mooc) ? learning_progress_mooc.length : 0;
+        console.log(`Progress: ${moocLength}/${course_content.length} MOOCs completed`);
 
         if (moocLength === 0) {
-            res.json(course_content[0]);
+            // Trả về MOOC đầu tiên với quantity
+            const firstMooc = course_content[0];
+            res.json({
+                data: firstMooc,
+                quantity: course_content.length
+            });
         } else if (moocLength > 0 && moocLength < course_content.length) {
-
-            res.json(course_content[moocLength]);
+            // Trả về MOOC tiếp theo với quantity
+            const nextMooc = course_content[moocLength];
+            res.json({
+                data: nextMooc,
+                quantity: course_content.length
+            });
         } else {
-            res.json({ message: "You have completed all the content of this course" });
+            // Đã hoàn thành tất cả MOOCs
+            res.json({ 
+                message: "You have completed all the content of this course",
+                quantity: course_content.length
+            });
         }
     } catch (error) {
         console.log('memberContinuesLearnCourseById error: ', error)
         res.status(500).json({ error: error.message || "Internal Server Error" })
     }
-
 }
 
 exports.createMemberEnrollmentCourse = async (req, res) => {
@@ -78,16 +95,18 @@ exports.createMemberEnrollmentCourse = async (req, res) => {
 
 exports.submitCourse = async (req, res) => {
     const { member_id, course_id, member_answer, version } = req.body;
+    console.log('1st log: ', req.body)
 
     try {
         const course = await courseModel.getCourseByIdAndVersion(course_id, version);
+
         if (!course) {
             return res.status(404).json({ error: "Course not found" });
         }
         const submittedCourse = await courseModel.calculateScoreMooc(course.content, member_answer);
-        if (submittedCourse.totalScore < 8) {
-            return res.status(400).json({ error: "You need to score at least 8 to complete this course" });
-        }
+        // if (submittedCourse.totalScore < 8) {
+        //     return res.status(400).json({ error: "You need to score at least 8 to complete this course" });
+        // }
         const learning_process = await courseModel.memberContinuesLearnCourseById(member_id, course_id);
         learning_process.learning_process.push(submittedCourse.MoocDetails);
         await courseModel.updateLearningProcess(member_id, course_id, learning_process.learning_process);
