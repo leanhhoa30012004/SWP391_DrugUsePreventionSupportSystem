bff5957873922e6222d4f34ba812ded919a67646
urjhn <uyennhi01022004@gmail.com>
2025-07-11 11:54:02 +0700

    communityprogram

--- swp391-client/src/App.jsx
+++ swp391-client/src/App.jsx
@@ -57,6 +57,12 @@ import ConsultantManagement from './pages/Manager/dashboard/ConsultantManagement
 import ConsultantBooking from './pages/Consultant/Consultant.jsx'
 import OnlineConsultant from './pages/Homepage/Onlineconsultant'
 import OnlyConsultant from './pages/OnlyConsultant/OnlyConsultant.jsx'
+
+
+import Programs from './pages/CommunityProgramme/ViewProgramme.jsx'
+import ProgramDetail from './pages/CommunityProgramme/DetailProgramme.jsx'
+import ProgramSurveyPage from './pages/CommunityProgramme/SurveyProgramme.jsx'
+
 function App() {
   return (
     <Routes>
@@ -102,6 +108,10 @@ function App() {
       <Route path="/consultant" element={<ConsultantBooking/>} />
       <Route path="/onlyconsultant" element={<OnlyConsultant/>}/>
 
+
+      <Route path="/programs" element={<Programs/>} />
+      <Route path="/programs/:program_id" element={<ProgramDetail/>} />
+      <Route path="/surveyprogram/:program_id" element={<ProgramSurveyPage/>} />
 
       {/* Admin Routes */}
       <Route path="/admin/dashboard" element={<AdminDashboardLayout />}>
