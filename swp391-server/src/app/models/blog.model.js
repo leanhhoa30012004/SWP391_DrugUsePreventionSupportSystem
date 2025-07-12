b921eec9d0bd7ec1ac3ca1717c81301d4882e64a
KhoaPhan204 <khoapvan204@gmail.com>
2025-07-12 17:48:33 +0700

    Add blog approval, rejection, like, and comment APIs
    Implemented endpoints and controller/model logic for blog approval, rejection with reason, liking/unliking, commenting, retrieving approved blogs, fetching like counts, listing comments, and removing comments. These changes enhance blog management and user interaction features.

--- swp391-server/src/app/models/blog.model.js
+++ swp391-server/src/app/models/blog.model.js
@@ -25,10 +25,10 @@ WHERE is_active = 1 AND blog_id = ?`, [manager_id, blog_id])
     return isApproved.affectedRows > 0
 }
 
-const rejectBlog = async (manager_id, blog_id) => {
+const rejectBlog = async (manager_id, blog_id, reject_reason) => {
     const [isReject] = await db.execute(`UPDATE Blog
-SET status = 'Rejected', approval_date = NOW(), manager_id = 5, reject_reason = 'bai viet qua nghien'
-WHERE is_active = 1 AND blog_id = 1`, [manager_id, blog_id])
+SET status = 'Rejected', approval_date = NOW(), manager_id = ?, reject_reason = ?
+WHERE is_active = 1 AND blog_id = ?`, [manager_id, reject_reason, blog_id])
     return isReject.affectedRows > 0
 }
 
@@ -38,6 +38,45 @@ FROM Blog WHERE blog_id = ? AND is_active =1`, [blog_id])
     return blog[0]
 }
 
+const getAllBlogAprroved = async () => {
+    const [list] = await db.execute(`SELECT b.blog_id, b.post_date, u.fullname, b.title, b.tags, b.cover_img, b.content
+FROM Blog b JOIN Users u ON b.author = u.user_id`)
+    return list;
+}
+
+const getLikeNumberByBlogId = async (blog_id) => {
+    const likeNumber = await db.execute(`SELECT COUNT(member_id ) AS 'Like number'
+FROM Like_blog WHERE blog_id = ?`, [blog_id]);
+    return likeNumber;
+}
+
+const likeBlog = async (member_id, blog_id) => {
+    const [isLike] = await db.execute(`INSERT INTO Like_blog  (member_id, blog_id, is_like)
+VALUES (?, ?, 1)
+ON DUPLICATE KEY UPDATE
+is_like = IF(is_like = 1, 0, 1);`, [member_id, blog_id])
+    return isLike.affectedRows > 0;
+}
+
+const commentBlog = async (member_id, blog_id, content) => {
+    const [isComment] = await db.execute(`INSERT INTO Comment_blog (member_id, blog_id, comment_date ,content)
+VALUES (?, ?, NOW(), ?)`, [member_id, blog_id, content])
+    return isComment.affectedRows > 0;
+}
+
+const getAllCommentByBlogId = async (blog_id) => {
+    const [comments] = await db.execute(`SELECT cb.comment_id, u.fullname, cb.blog_id, cb.comment_date, cb.content
+FROM Comment_blog cb JOIN Users u ON cb.member_id = u.user_id
+WHERE cb.is_active = 1 AND cb.blog_id = ?`, [blog_id])
+    return comments
+}
+
+const removeCommentByCommentId = async (comment_id) => {
+    const [isRemove] = await db.execute(`UPDATE Comment_blog
+SET is_active = 0 WHERE comment_id = ?`, [comment_id])
+    return isRemove.affectedRows > 0
+}
+
 module.exports = {
     blogPost,
     getAllBlogPending,
@@ -45,4 +84,10 @@ module.exports = {
     approvalBlog,
     rejectBlog,
     getBlogById,
+    getAllBlogAprroved,
+    likeBlog,
+    getLikeNumberByBlogId,
+    commentBlog,
+    getAllCommentByBlogId,
+    removeCommentByCommentId
 }
