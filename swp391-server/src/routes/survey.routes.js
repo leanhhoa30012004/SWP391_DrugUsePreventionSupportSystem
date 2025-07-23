66936f8bfd1b845f8aefe72420ac3b001e73efaf
KhoaPhan204 <khoapvan204@gmail.com>
2025-07-24 02:57:00 +0700

    Add API to fetch detailed survey history by enrollment ID
    Introduces a new endpoint to retrieve detailed survey history for a given survey enrollment ID, including survey content and member answers. Adds supporting model functions and controller logic to handle the request and response.

--- swp391-server/src/routes/survey.routes.js
+++ swp391-server/src/routes/survey.routes.js
@@ -8,4 +8,5 @@ router.get("/survey-by-id/:survey_id", survey.findSurveyBySurveyId);
 router.post("/submit-survey", survey.submitSurvey);
 router.get("/survey-history/:member_id", survey.getSurveyHistoryByMember);
 router.get("/get-all-survey-follow-survey-enrollment-by-member-id/:member_id", survey.getAllSurveyFollowSurveyEnrollmentByMemberId)
+router.get("/survey-history-by-survey-enrollment-id/:survey_enrollment_id", survey.getDetailSurveyHistoryBySurveyEnrollmenId);
 module.exports = router;
